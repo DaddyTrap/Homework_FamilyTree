2d251d5af29095486645b760c623ef754d3bff31
DaddyTrap <daddytrapc@gmail.com>
2016-11-10 21:23:12 +0800

    Initial commit

---
+++ Makefile
@@ -0,0 +1,23 @@
+CC := g++
+FLAGS := -std=c++11 -g -Wall
+INC_DIR := include
+SRC_DIR := src
+BIN_DIR := bin
+BUILD_DIR := build
+INCLUDE := -I./$(INC_DIR)
+
+$(BIN_DIR)/FamilyTree: $(BUILD_DIR)/FamilyTree.o $(BUILD_DIR)/FamilyManager.o
+	mkdir -p $(BIN_DIR)
+	$(CC) $(FLAGS) $(INCLUDE) $^ -o $@
+
+$(BUILD_DIR)/FamilyTree.o: $(SRC_DIR)/FamilyTree.cpp
+	mkdir -p $(BUILD_DIR)
+	$(CC) $(FLAGS) $(INCLUDE) $^ -c -o $@
+
+$(BUILD_DIR)/FamilyManager.o: $(SRC_DIR)/FamilyManager.cpp
+	mkdir -p $(BUILD_DIR)
+	$(CC) $(FLAGS) $(INCLUDE) $^ -c -o $@
+
+clean:
+	rm -rf $(BUILD_DIR)
+	rm -rf $(BIN_DIR)
---
+++ include/FamilyTree.hpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <functional>
+#include <vector>
+
+using std::string;
+using std::function;
+using std::vector;
+
+class FamilyTree {
+ public:
+  enum Sex {
+    boy,
+    girl,
+    unknown
+  };
+
+  struct FamilyMember {
+    int id;
+    string name;
+    Sex sex;
+    int age;
+    bool dead;
+    bool divorced;
+    FamilyMember *left;
+    FamilyMember *right;
+    FamilyMember(int t_id, string t_name, Sex t_sex, int t_age, bool t_die, FamilyMember *t_left, FamilyMember *t_right);
+  };
+
+  FamilyTree();
+
+  ~FamilyTree();
+
+  void insertChild(FamilyMember *person, FamilyMember *mother);
+
+  void insertWife(FamilyMember *person, FamilyMember *husband);
+
+  bool deleteMember(function<bool(const FamilyMember *person)> filter);
+
+  vector<FamilyMember*> queryMember(function<bool(const FamilyMember *person)> filter);
+
+  bool updateMember(function<bool(const FamilyMember *person)> filter, function<void(FamilyMember *person)> switcher);
+
+  size_t countMembers(function<bool(const FamilyMember *person)> filter);
+
+  static void PrintMembers(const FamilyMember* root, int printLevel, bool isLeft, bool hasRight);
+
+};
